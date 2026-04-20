809c7c117538215fa12a6befe33d6edc9db1de39
AnjaloDanuzikan <dayofcertify2024@gmail.com>
2026-04-20 10:17:27 +0530

    Added one new question

--- sif2500026.py
+++ sif2500026.py
@@ -26,14 +26,20 @@ if q3.lower() == "python":
 else:
     print("Wrong! The correct answer is Python.\n")
 
+q4 = input("4.What does 'MB' stand for? ")
+if q4.lower() == "megabyte":
+    print("Correct!\n")
+    score += 1
+else:
+    print("Wrong! The correct answer is Megabyte.\n")
+
 print("Your final score is:", score)
 
-if score == 3:
+if score == 4:
     print("Excellent! You handled all scenarios perfectly.")
+elif score == 3:
+    print("Good job! Just one mistake.")
 elif score == 2:
-    print("Good job! Just a small mistake.")
-elif score == 1:
     print("You need more practice.")
 else:
     print("Try again and improve your knowledge.")
-
